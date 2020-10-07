97ec3504f2a2ba6b1c82086dc88966a9664b5748
Junyeong Park <jyp10987@gmail.com>
2020-10-07 17:51:10 +0900

    feat(project2): implement file manager

--- project2/include/dbapi.h
+++ project2/include/dbapi.h
@@ -7,7 +7,7 @@ int open_table(char* pathname);
 
 int db_insert(int64_t key, char* value);
 
-int db_fild(int64_t key, char* ret_val);
+int db_find(int64_t key, char* ret_val);
 
 int db_delete(int64_t key);
 
--- project2/include/file.h
+++ project2/include/file.h
@@ -1,60 +1,84 @@
 #ifndef __PAGE_H__
 #define __PAGE_H__
 
-#include <stdint.h>
+#include <cstdint>
+#include <string>
 
 // SIZE CONSTANTS
-#define PAGE_DATA_VALUE_SIZE 120
+constexpr size_t PAGE_DATA_VALUE_SIZE = 120;
 
-#define PAGE_HEADER_SIZE 128
-#define PAGE_HEADER_USED 16
-#define PAGE_HEADER_RESERVED (PAGE_HEADER_SIZE - PAGE_HEADER_USED)
+constexpr size_t PAGE_HEADER_SIZE = 128;
+constexpr size_t PAGE_HEADER_USED = 16;
+constexpr size_t PAGE_HEADER_RESERVED = PAGE_HEADER_SIZE - PAGE_HEADER_USED;
 
-#define PAGE_SIZE 4096
-#define PAGE_DATA_IN_PAGE 31
-#define PAGE_BRANCHES_IN_PAGE 248
+constexpr size_t PAGE_SIZE = 4096;
+constexpr size_t PAGE_DATA_IN_PAGE = 31;
+constexpr size_t PAGE_BRANCHES_IN_PAGE = 248;
 
-#define HEADER_PAGE_USED 24
-#define HEADER_PAGE_RESERVED (PAGE_SIZE - HEADER_PAGE_USED)
+constexpr size_t HEADER_PAGE_USED = 24;
+constexpr size_t HEADER_PAGE_RESERVED = PAGE_SIZE - HEADER_PAGE_USED;
 
 // TYPES
-typedef uint64_t pagenum_t;
+using pagenum_t = uint64_t;
 
-typedef struct page_data_t {
+struct page_data_t {
     int64_t key;
     char value[PAGE_DATA_VALUE_SIZE];
-} page_data_t;
+};
 
-typedef struct page_branch_t {
+struct page_branch_t {
     int64_t key;
     pagenum_t child_page_id;
-} page_branch_t;
+};
 
-typedef struct page_header_t {
+struct page_header_t {
     pagenum_t next_free_page_id;
 
     int is_leaf;
     int num_keys;
 
     char reserved[PAGE_HEADER_RESERVED];
-} page_header_t;
+};
 
-typedef struct page_t {
+struct page_t {
     page_header_t header;
 
     union {
         page_data_t data[PAGE_DATA_IN_PAGE];
         page_branch_t branch[PAGE_BRANCHES_IN_PAGE];
     };
-} page_t;
+};
 
-typedef struct header_page_t {
+struct header_page_t {
     uint64_t free_page_number;
     uint64_t root_page_number;
     uint64_t num_pages;
 
     char reserved[HEADER_PAGE_RESERVED];
-} header_page_t;
+};
+
+class FileManager final
+{
+public:
+    static FileManager& get();
+
+    ~FileManager();
+
+    bool open(const std::string& filename);
+    void close();
+
+    bool is_open() const;
+
+    bool read(size_t size, size_t offset, void* value);
+    bool write(size_t size, size_t offset, const void* value);
+    
+    header_page_t* header() const;
+    void update_header();
+
+private:
+    int file_handle_{ -1 };
+    header_page_t* header_{ nullptr };
+};
 
 // Allocate an on-disk page from the free page list
 pagenum_t file_alloc_page();
