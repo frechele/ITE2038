ab04439828bad6f4e17197e84917780d69d29176
Junyeong Park <jyp10987@gmail.com>
2020-11-12 00:40:58 +0900

    fix: fix race condition

--- project4/src/lock_table.cpp
+++ project4/src/lock_table.cpp
@@ -5,12 +5,14 @@
 #include <condition_variable>
 #include <mutex>
 #include <tuple>
+#include <string>
 #include <unordered_map>
 
 class HashTableEntry;
 
 struct lock_t {
-	std::condition_variable cond;
+	bool wait{ false };
+	std::condition_variable* cond{ nullptr };
 
 	HashTableEntry* entry{ nullptr };
 	lock_t* prev{ nullptr };
@@ -26,9 +28,9 @@ struct hash<table_record_t>
 {
 	size_t operator()(const table_record_t& key) const
 	{
-		auto [tid, pid] = key;
+		auto [tid, rid] = key;
 
-		return static_cast<size_t>(tid) << 32 | pid;
+		return std::hash<string>()(std::to_string(tid) + '|' + std::to_string(rid));
 	}
 };
 }
@@ -36,23 +38,42 @@ struct hash<table_record_t>
 class HashTableEntry final
 {
 public:
-	[[nodiscard]] lock_t* acquire();
+	HashTableEntry(table_record_t trid);
+
+	const table_record_t& table_record_id() const;
+
+	[[nodiscard]] lock_t* update_link();
+	void wait(lock_t* lock_obj);
 	void release(lock_t* lock_obj);
 
 private:
+	table_record_t trid_;
+
 	std::mutex mutex_;
+	std::condition_variable cond_;
 
 	lock_t* head_{ nullptr };
 	lock_t* tail_{ nullptr };
 };
 
-lock_t* HashTableEntry::acquire()
+HashTableEntry::HashTableEntry(table_record_t trid)
+	: trid_(std::move(trid))
 {
-	std::unique_lock lock(mutex_);
+}
+
+const table_record_t& HashTableEntry::table_record_id() const
+{
+	return trid_;
+}
+
+lock_t* HashTableEntry::update_link()
+{
+	std::scoped_lock lock(mutex_);
 
 	lock_t* lock_obj = new (std::nothrow) lock_t;
 	CHECK_FAILURE2(lock_obj != nullptr, nullptr);
 
+	lock_obj->cond = &cond_;
 	lock_obj->entry = this;
 
 	if (tail_ == nullptr)
@@ -69,10 +90,17 @@ lock_t* HashTableEntry::acquire()
 
 		tail_ = lock_obj;
 
-		lock_obj->cond.wait(lock);
+		lock_obj->wait = true;
 	}
 	
 	return lock_obj;
+}
+
+void HashTableEntry::wait(lock_t* lock_obj)
+{
+	std::unique_lock lock(mutex_);
+
+	lock_obj->cond->wait(lock, [lock_obj]{ return !lock_obj->wait; });
 }
 
 void HashTableEntry::release(lock_t* lock_obj)
@@ -99,8 +127,11 @@ void HashTableEntry::release(lock_t* lock_obj)
 
 	delete lock_obj;
 
-	if (head_ != nullptr)
-		head_->cond.notify_all();
+	if (head_ == nullptr)
+		return;
+
+	head_->wait = false;
+	cond_.notify_all();
 }
 
 class LockTableManager final
@@ -113,6 +144,9 @@ public:
 
 	[[nodiscard]] lock_t* acquire(int table_id, int64_t key);
 	[[nodiscard]] bool release(lock_t* lock_obj);
+
+private:
+	void clear_all_locks();
 
 private:
 	inline static LockTableManager* instance_{ nullptr };
@@ -130,7 +164,7 @@ bool LockTableManager::initialize()
 {
 	CHECK_FAILURE(instance_ == nullptr);
 
-	instance_ = new (std::nothrow) LockTableManager();
+	instance_ = new (std::nothrow) LockTableManager;
 	CHECK_FAILURE(instance_ != nullptr);
 
 	return true;
@@ -139,6 +173,8 @@ bool LockTableManager::initialize()
 bool LockTableManager::shutdown()
 {
 	CHECK_FAILURE(instance_ != nullptr);
+
+	instance_->clear_all_locks();
 
 	delete instance_;
 	instance_ = nullptr;
@@ -155,17 +191,18 @@ LockTableManager& LockTableManager::get_instance()
 
 lock_t* LockTableManager::acquire(int table_id, int64_t key)
 {
-	table_record_t trid{ table_id, key };
-
+	lock_t* lock_obj;
 	HashTableEntry* entry;
 
 	{
 		std::scoped_lock lock(table_latch_);
 
+		table_record_t trid{ table_id, key };
+
 		auto it = locks_.find(trid);
 		if (it == end(locks_))
 		{
-			entry = new (std::nothrow) HashTableEntry;
+			entry = new (std::nothrow) HashTableEntry(trid);
 			CHECK_FAILURE2(entry != nullptr, nullptr);
 
 			locks_[trid] = entry;
@@ -174,9 +211,14 @@ lock_t* LockTableManager::acquire(int table_id, int64_t key)
 		{
 			entry = it->second;
 		}
+
+		lock_obj = entry->update_link();
 	}
 
-	return entry->acquire();
+	if (lock_obj != nullptr)
+		entry->wait(lock_obj);
+
+	return lock_obj;
 }
 
 bool LockTableManager::release(lock_t* lock_obj)
@@ -186,9 +228,18 @@ bool LockTableManager::release(lock_t* lock_obj)
 	std::scoped_lock lock(table_latch_);
 
 	HashTableEntry* entry = lock_obj->entry;
+	
 	entry->release(lock_obj);
 
 	return true;
+}
+
+void LockTableManager::clear_all_locks()
+{
+	for (auto& pr : locks_)
+	{
+		delete pr.second;
+	}
 }
 
 int init_lock_table()
