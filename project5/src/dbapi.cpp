1fa0665d16f7d9add715b29cbf7efea37fe4568f
Junyeong Park <jyp10987@gmail.com>
2020-11-28 18:59:13 +0900

    fix: fix dbapi

--- project5/src/dbapi.cpp
+++ project5/src/dbapi.cpp
@@ -1,7 +1,10 @@
 #include "dbapi.h"
 
 #include "common.h"
+#include "lock.h"
+#include "log.h"
 #include "table.h"
+#include "xact.h"
 
 #include <cstring>
 
@@ -9,6 +12,9 @@
 
 int init_db(int num_buf)
 {
+    CHECK_FAILURE2(LockManager::initialize(), FAIL);
+    CHECK_FAILURE2(LogManager::initialize(), FAIL);
+    CHECK_FAILURE2(XactManager::initialize(), FAIL);
     CHECK_FAILURE2(TableManager::initialize(num_buf), FAIL);
 
     return SUCCESS;
@@ -17,14 +23,16 @@ int init_db(int num_buf)
 int shutdown_db()
 {
     CHECK_FAILURE2(TableManager::shutdown(), FAIL);
+    CHECK_FAILURE2(XactManager::shutdown(), FAIL);
+    CHECK_FAILURE2(LogManager::shutdown(), FAIL);
+    CHECK_FAILURE2(LockManager::shutdown(), FAIL);
 
     return SUCCESS;
 }
 
 int open_table(char* pathname)
 {
-    if (!TableManager::is_initialized())
-        return -1;
+    CHECK_FAILURE2(TableManager::is_initialized(), -1);
 
     if (auto table_id = TblMgr().open_table(pathname); table_id.has_value())
         return table_id.value();
@@ -34,71 +42,90 @@ int open_table(char* pathname)
 
 int close_table(int table_id)
 {
-    if (!TableManager::is_initialized())
-        return FAIL;
+    CHECK_FAILURE2(TableManager::is_initialized(), FAIL);
 
     return TblMgr().close_table(table_id) ? SUCCESS : FAIL;
 }
 
 int db_insert(int table_id, int64_t key, char* value)
 {
-    if (!TableManager::is_initialized())
-        return FAIL;
+    CHECK_FAILURE2(TableManager::is_initialized(), FAIL);
 
     auto table = TblMgr().get_table(table_id);
-
-    if (!table.has_value())
-        return FAIL;
+    CHECK_FAILURE2(table.has_value(), FAIL);
 
     page_data_t record;
     record.key = key;
     strncpy(record.value, value, PAGE_DATA_VALUE_SIZE);
 
-    return table.value()->insert(record) ? SUCCESS : FAIL;
+    CHECK_FAILURE2(table.value()->insert(record), FAIL);
+
+    return SUCCESS;
 }
 
 int db_find(int table_id, int64_t key, char* ret_val, int trx_id)
 {
-    if (!TableManager::is_initialized())
-        return FAIL;
+    CHECK_FAILURE2(TableManager::is_initialized(), FAIL);
 
     auto table = TblMgr().get_table(table_id);
+    CHECK_FAILURE2(table.has_value(), FAIL);
 
-    if (!table.has_value())
-        return FAIL;
+    Xact* xact = XactMgr().get(trx_id);
+    CHECK_FAILURE2(xact != nullptr, FAIL);
 
-    auto res = table.value()->find(key);
-    if (!res)
-        return FAIL;
+    auto res = table.value()->find(key, xact);
+    CHECK_FAILURE2(res.has_value(), FAIL);
 
     strncpy(ret_val, res.value().value, PAGE_DATA_VALUE_SIZE);
+
     return SUCCESS;
 }
 
 int db_delete(int table_id, int64_t key)
 {
-    if (!TableManager::is_initialized())
-        return FAIL;
+    CHECK_FAILURE2(TableManager::is_initialized(), FAIL);
 
     auto table = TblMgr().get_table(table_id);
+    CHECK_FAILURE2(table.has_value(), FAIL);
 
-    if (!table.has_value())
-        return FAIL;
+    CHECK_FAILURE2(table.value()->remove(key), FAIL);
 
-    return table.value()->remove(key) ? SUCCESS : FAIL;
+    return SUCCESS;
 }
 
-int db_update(int table_id, int64_t key, char* values, int trx_id)
+int db_update(int table_id, int64_t key, char* value, int trx_id)
 {
-    return FAIL;
+    CHECK_FAILURE2(TableManager::is_initialized(), FAIL);
+
+    auto table = TblMgr().get_table(table_id);
+    CHECK_FAILURE2(table.has_value(), FAIL);
+
+    Xact* xact = XactMgr().get(trx_id);
+    CHECK_FAILURE2(xact != nullptr, FAIL);
+
+    CHECK_FAILURE2(table.value()->update(key, value, xact), FAIL);
+
+    return SUCCESS;
 }
 
 int trx_begin()
 {
-    return 0;
+    CHECK_FAILURE2(TableManager::is_initialized(), 0);
+
+    Xact* xact = XactMgr().begin();
+    CHECK_FAILURE2(xact != nullptr, 0);
+
+    return xact->id();
 }
 
 int trx_commit(int trx_id)
 {
-    return 0;
+    CHECK_FAILURE2(TableManager::is_initialized(), 0);
+
+    Xact* xact = XactMgr().get(trx_id);
+    CHECK_FAILURE2(xact != nullptr, 0);
+
+    CHECK_FAILURE2(XactMgr().commit(xact), 0);
+
+    return trx_id;
 }
