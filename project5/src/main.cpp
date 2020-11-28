7d3abd46907cb597fcebda9752da35dee9110e62
Junyeong Park <jyp10987@gmail.com>
2020-11-28 23:39:27 +0900

    test: add unittests for project5

--- project5/src/main.cpp
+++ project5/src/main.cpp
@@ -2,192 +2,149 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstring>
 
-// for performance test
-#include <algorithm>
 #include <chrono>
-#include <random>
-#include <vector>
+#include <thread>
 
 #include "common.h"
 #include "dbapi.h"
-#include "buffer.h"
-
-#include "bpt.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-#define PERF_TEST
+void test1()
+{
+    int trx_id = trx_begin();
+
+    assert(trx_commit(trx_id) == trx_id);
+}
+
+void test2(int tid)
+{
+    int trx_id = trx_begin();
+    assert(trx_id == 2);
+
+    char value[120];
+    assert(SUCCESSED(db_find(tid, 1, value, trx_id)));
+
+    assert(trx_commit(trx_id) == trx_id);
+}
+
+void test3(int tid)
+{
+    int trx_id = trx_begin();
+    assert(trx_id == 3);
+
+    char value[120];
+    assert(SUCCESSED(db_find(tid, 1, value, trx_id)));
+    assert(SUCCESSED(db_find(tid, 2, value, trx_id)));
+    assert(SUCCESSED(db_find(tid, 3, value, trx_id)));
+
+    {
+        char tmp[120] = "Hello World! 1";
+        assert(SUCCESSED(db_update(tid, 1, tmp, trx_id)));
+        assert(SUCCESSED(db_find(tid, 1, value, trx_id)));
+        assert(strcmp(tmp, value) == 0);
+    }
+
+    {
+        char tmp[120] = "Hello World! 2";
+        assert(SUCCESSED(db_update(tid, 2, tmp, trx_id)));
+        assert(SUCCESSED(db_find(tid, 2, value, trx_id)));
+        assert(strcmp(tmp, value) == 0);
+    }
+
+    {
+        char tmp[120] = "Hello World! 3";
+        assert(SUCCESSED(db_update(tid, 3, tmp, trx_id)));
+        assert(SUCCESSED(db_find(tid, 3, value, trx_id)));
+        assert(strcmp(tmp, value) == 0);
+    }
+
+    assert(trx_commit(trx_id) == trx_id);
+}
+
+void test4(int tid)
+{
+    int trx1 = trx_begin();
+    assert(trx1 == 4);
+
+
+    char str_update_by_trx1_1[120] = "UPDATE_BY_TRX1_1";
+    char str_update_by_trx1_2[120] = "UPDATE_BY_TRX1_2";
+    char value[120];
+
+    std::thread worker2([tid] {
+        int trx2 = trx_begin();
+        assert(trx2 == 5);
+
+        char value[120];
+        char str_update_by_trx2_2[120] = "UPDATE_BY_TRX2_2";
+
+        assert(SUCCESSED(db_find(tid, 1, value, trx2)));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+        assert(FAILED(db_update(tid, 2, str_update_by_trx2_2, trx2)));
+        assert(FAILED(db_find(tid, 2, value, trx2)));
+        assert(trx_commit(trx2) == 0);
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    assert(SUCCESSED(db_update(tid, 2, str_update_by_trx1_2, trx1)));
+    assert(SUCCESSED(db_update(tid, 1, str_update_by_trx1_1, trx1)));
+
+    if (worker2.joinable())
+        worker2.join();
+
+    assert(SUCCESSED(db_find(tid, 1, value, trx1)));
+    assert(strcmp(value, str_update_by_trx1_1) == 0);
+    assert(SUCCESSED(db_find(tid, 2, value, trx1)));
+    assert(strcmp(value, str_update_by_trx1_2) == 0);
+
+    assert(trx_commit(trx1) == trx1);
+}
+
+template <typename Func, typename... Args>
+void do_test(int test_id, const std::string& desc, Func&& func, Args&&... args)
+{
+    std::cerr << "[test " << test_id << "] " << desc << "\n";
+
+    const auto start_point = std::chrono::system_clock::now();
+
+    func(std::forward<Args>(args)...);
+
+    const auto end_point = std::chrono::system_clock::now();
+
+    std::cerr << "time: "
+                << std::chrono::duration_cast<std::chrono::milliseconds>(
+                        end_point - start_point)
+                        .count()
+                << " ms\n" << endl;
+}
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-
-    if (FAILED(init_db(3)))
+    if (FAILED(init_db(1000)))
     {
-#ifndef PERF_TEST
-        cout << "ERROR: cannot init db" << endl;
-#endif
-        return -1;
+        cout << "cannot init db" << endl;
+        return 1;
     }
-
-#ifndef PERF_TEST
-    bool is_running = true;
-
-    std::istream& in_stream = cin;
-    // std::istream&& in_stream = std::ifstream("input.txt");
-
-    while (!in_stream.eof() && is_running)
-    {
-        std::string cmd;
-        in_stream >> cmd;
-
-        int table_id;
-
-        if (cmd == "open")
-        {
-            std::string arg;
-            in_stream >> arg;
-
-            table_id = open_table(&arg[0]);
-            if (table_id == -1)
-            {
-                cout << "cannot open table " << arg << '.' << endl;
-            }
-            else
-            {
-                cout << "open table " << arg << " (id : " << table_id << ")."
-                     << endl;
-            }
-        }
-        else if (cmd == "close")
-        {
-            cin >> table_id;
-
-            if (SUCCESSED(close_table(table_id)))
-            {
-                cout << "close table successed." << endl;
-            }
-            else
-            {
-                cout << "close table failed." << endl;
-            }
-        }
-        else if (cmd == "quit")
-        {
-            cout << "dbms will be shutdown." << endl;
-            is_running = false;
-        }
-        else if (cmd == "insert")
-        {
-            int64_t key;
-            std::string value;
-            in_stream >> table_id >> key;
-            in_stream.get();
-            std::getline(in_stream, value);
-
-            if (SUCCESSED(db_insert(table_id, key, &value[0])))
-            {
-                cout << "insert " << key << " successed." << endl;
-            }
-            else
-            {
-                cout << "insert " << key << " failed." << endl;
-            }
-        }
-        else if (cmd == "find")
-        {
-            int64_t key;
-            in_stream >> table_id >> key;
-
-            char value[120];
-            if (SUCCESSED(db_find(table_id, key, value)))
-            {
-                cout << key << " : " << value << endl;
-            }
-            else
-            {
-                cout << key << " is not found." << endl;
-            }
-        }
-        else if (cmd == "delete")
-        {
-            int64_t key;
-            in_stream >> table_id >> key;
-
-            if (SUCCESSED(db_delete(table_id, key)))
-            {
-                cout << "delete " << key << " successed." << endl;
-            }
-            else
-            {
-                cout << "delete " << key << " failed." << endl;
-            }
-        }
-        else if (cmd == "sep")
-        {
-            cout << "sep" << endl;
-        }
-    }
-#else
-    const int N = 10000;
-
-    //std::random_device rd;
-    std::mt19937 engine(123456);
 
     const int tid = open_table("/mnt/ssd/dbms/test1.db");
     assert(tid != -1);
 
+    do_test(1, "only trx begin and commit", test1);
+    do_test(2, "trx with find", test2, tid);
+    do_test(3, "multiple find and update", test3, tid);
+    do_test(4, "deadlock detection", test4, tid);
+
+    if (FAILED(shutdown_db()))
     {
-        std::vector<int> keys(N);
-        std::iota(begin(keys), end(keys), 1);
-        std::shuffle(begin(keys), end(keys), engine);
-
-        std::vector<std::string> values(N);
-        std::transform(begin(keys), end(keys), begin(values),
-                    [](int k) { return std::to_string(k); });
-
-        const auto start_point = std::chrono::system_clock::now();
-
-        for (int i = 0; i < N; ++i) {
-            assert(db_insert(tid, keys[i], &values[i][0]) == SUCCESS);
-        }
-
-        const auto end_point = std::chrono::system_clock::now();
-
-        std::cerr << "time: "
-                << std::chrono::duration_cast<std::chrono::milliseconds>(
-                        end_point - start_point)
-                        .count()
-                << " ms" << endl;
+        cout << "cannot shutdown db" << endl;
+        return 1;
     }
-
-    {
-        const int delete_N = N / 10;
-
-        std::vector<int> keys(delete_N);
-        std::iota(begin(keys), end(keys), 1);
-        std::shuffle(begin(keys), end(keys), engine);
-
-        const auto start_point = std::chrono::system_clock::now();
-
-        for (int i = 0; i < delete_N; ++i)
-        {
-            assert(db_delete(tid, keys[i]) == SUCCESS);
-        }
-
-        const auto end_point = std::chrono::system_clock::now();
-
-        std::cerr << "time: "
-                << std::chrono::duration_cast<std::chrono::milliseconds>(
-                        end_point - start_point)
-                        .count()
-                << " ms" << endl;
-    }
-
-#endif
-    shutdown_db();
 }
